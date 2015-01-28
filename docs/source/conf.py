57e394938a33e078a09f66ca385e940e391e7bef
swinepig <409989258@qq.com>
2015-01-28 12:56:29 +0800

    8

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -118,7 +118,7 @@ html_theme_path = ['../sphinxStyle']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = 'swinepig doc'
+html_title = 'Home'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
