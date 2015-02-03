976b3426a674bccab1dda07176f7b702b57c91dd
swinepig <409989258@qq.com>
2015-02-03 12:07:32 +0800

    优化代码，修复BUG

---
+++ docs/sphinxStyle/der/static/sidebar.js
@@ -0,0 +1,54 @@
+/*
+ * sidebar.js
+ * ~~~~~~~~~~
+ */
+
+$(function() {
+
+    var bodywrapper = $('.documentwrapper');
+    var navigationprev = $('.navigation.navigation-prev');
+    var nav = $('.wy-grid-for-nav');
+    $('.sibebarz').click(check);
+    set_position_from_cookie();
+    function check() {
+
+        if (nav.css('display') == 'block') {
+            bodywrapper.animate({
+                left: "0px"
+            },
+            "slow");
+            navigationprev.css('left', '10px');
+            nav.fadeOut("slow");
+            document.cookie = 'sidebar=collapsed';
+        } else {
+            bodywrapper.animate({
+                left: "270px"
+            },
+            "slow");
+            navigationprev.css('left', '270px');
+            nav.fadeIn("slow");
+            document.cookie = 'sidebar=expanded';
+        }
+    }
+
+    function set_position_from_cookie() {
+        if (!document.cookie) return;
+        var items = document.cookie.split(';');
+        for (var k = 0; k < items.length; k++) {
+            var key_val = items[k].split('=');
+            var key = key_val[0];
+            if (key == 'sidebar') {
+                var value = key_val[1];
+                if ((value == 'collapsed')) {
+                    bodywrapper.css('left', '0px');
+                    navigationprev.css('left', '10px');
+                    nav.hide();
+                } else if ((value == 'expanded')) {
+                    bodywrapper.css('left', '270px');
+                    navigationprev.css('left', '270px');
+                    nav.show();
+                }
+            }
+        }
+    }
+});
