17429538e9813ba64e09898dbfd7aa7c9125f837
swinepig <409989258@qq.com>
2015-11-12 16:51:15 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'swinepig'
-copyright = u'2015, Zhuxin. Last updated on Nov 05, 2015'
+copyright = u'2015, Zhuxin.'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
